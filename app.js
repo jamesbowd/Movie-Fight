e6a3f00a3b3f8eb8e51dc211ae663ca10d3c0c62
jamesbowd <jamesbowd@gmail.com>
2020-06-22 20:58:39 +0100

    setup debounce

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com", {
     params: {
       apikey: "44e15dbc",
-      i: "tt0848228",
+      s: searchTerm,
     },
   });
 
-  console.log(response.data);
+  return response.data.Search;
 };
 
-fetchData();
+const input = document.querySelector("input");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value); // This is what the user has typed into the input
+  console.log(movies);
+};
+input.addEventListener("input", debounce(onInput, 500));
